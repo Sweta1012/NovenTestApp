66e1b51156ded46967789869fd6b40eb5f2c6c57
Sweta <swetap1012@gmail.com>
2019-08-14 13:12:10 -0400

    chart added

--- novenTestApp/src/app/chart/chart.component.ts
+++ novenTestApp/src/app/chart/chart.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  Input, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { chart } from 'highcharts';
 
 @Component({
   selector: 'app-chart',
@@ -7,9 +8,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartComponent implements OnInit {
 
+  usersGraph:any;
+	seriesData = [];
+	@Input() TodoGraphData;
+	flag = false;
+	@ViewChild('chartTarget', {}) chartTarget: ElementRef;
+
   constructor() { }
 
+  ngOnChanges(simpleChanges){
+		if(this.flag){
+			this.TodoGraphData = simpleChanges.TodoGraphData.currentValue;
+			this.getGraphData();
+		}
+	}
+
   ngOnInit() {
+    this.flag = true;
+		this.getGraphData();
   }
+  getGraphData(){
+		this.usersGraph = {
+			chart: {
+				type: 'bar'
+			},
+			title: {
+				text: 'User Todo List'
+			},
+			xAxis: {
+				categories: ['Completed', 'Pending'],
+				crosshair: true
+			},
+			yAxis: {
+				title: {
+					text: 'Todo List Summary'
+				}
+			},
+			plotOptions: {
+				column: {
+					pointPadding: 0.4,
+					dataLabels: {
+						enabled: true,
+						format: '<b>{point.y}</b>'
+					}
+
+				},
+				bar: {
+					pointPadding: 0.3,
+					groupPadding: 0.2,
+					dataLabels: {
+						enabled: true,
+						format: '<b>{point.y}</b>',
+						allowOverlap:false
+					}
+				},
+				pie: {
+					allowPointSelect: true,
+					cursor: 'pointer',
+					dataLabels: {
+						enabled: true,
+						format: '{point.name}: <b>{point.y}</b>'
+					}
+				},
+				line: {
+					dataLabels: {
+						enabled: true,
+						format: '<b>{point.name}</b>: {point.y:.2f} ',
+					}
+				}
+			},
+			series: this.TodoGraphData
+		};
+		chart(this.chartTarget.nativeElement, this.usersGraph);
+	} 
 
 }
--- novenTestApp/src/app/users/users.component.ts
+++ novenTestApp/src/app/users/users.component.ts
@@ -7,60 +7,97 @@ import { UserServiceService } from '../services/user-service.service';
 })
 export class UsersComponent implements OnInit {
 
-  constructor(private apiService: UserServiceService) { }
-
-  usersArray:any = [];
+	constructor(private apiService: UserServiceService) { }
+	usersArray:any = [];
 	usersEmptyArray:boolean = false;
 	allTodoList:any = [];
 	allTodosEmpty:boolean = false;
 	selectedUser:any;
 	completedArray = [];
-  pendingArray = [];
-  type:any;
+	pendingArray = [];
+	selectUsergraphData = [];
+	showGraph = false;
+	isDesc: boolean = true;
+	column: string = 'id';
+	type:any;
 	staticData:any = [];
 
-  ngOnInit() {
+	ngOnInit() {
 
-      this.usersList();
-      this.apiService.getUserDetails().subscribe(res => {
-        // console.log("response:: " ,res);
-        this.allTodoList = res;
-        // console.log("allToDoList:: " ,this.allTodoList);
+		this.usersList();
+		this.apiService.getUserDetails().subscribe(res => {
+			// console.log("response:: " ,res);
+			this.allTodoList = res;
+			// console.log("allToDoList:: " ,this.allTodoList);
 
-        if(this.allTodoList.length == 0){
-          this.allTodosEmpty = true;
-          return false;
-        }
-        setTimeout(()=>{this.getUserDetails(this.usersArray[0])},500)
-      })
-    }
+			if(this.allTodoList.length == 0){
+				this.allTodosEmpty = true;
+				return false;
+			}
+			setTimeout(()=>{this.getUserDetails(this.usersArray[0])},500)
+		})
 
-      usersList(){
-          this.apiService.getUsers().subscribe(res =>{
-              this.staticData = res;
-              this.staticData.forEach(resp => resp['isSelected'] = false);
-              this.staticData[0]['isSelected'] = true;
-              this.usersArray = this.staticData;
-              if(this.usersArray.length == 0){
-                this.usersEmptyArray = true;
-                return false;
-              }
-      
-          })
-      }
+	}
 
-      getUserDetails(data){
-          this.usersArray.forEach(res => {
-            (res['id'] == data.id) ? res['isSelected'] = true : res['isSelected'] = false;
-          });
-      
-          this.completedArray = [];
-          this.pendingArray = [];
-          this.selectedUser = this.allTodoList.filter(res => res['userId'] == data.id);
-          this.selectedUser.forEach(res => {
-              (res['completed'] == true) ? this.completedArray.push(res) : this.pendingArray.push(res);
-          });
-          console.log('userDetails::::::43', this.selectedUser, this.completedArray, this.pendingArray);
-      }
+	usersList(){
+		this.apiService.getUsers().subscribe(res =>{
+			this.staticData = res;
+			this.staticData.forEach(resp => resp['isSelected'] = false);
+			this.staticData[0]['isSelected'] = true;
+			this.usersArray = this.staticData;
+			if(this.usersArray.length == 0){
+				this.usersEmptyArray = true;
+				return false;
+			}
 
+		})
+	}
+
+	getUserDetails(data){
+		this.usersArray.forEach(res => {
+			(res['id'] == data.id) ? res['isSelected'] = true : res['isSelected'] = false;
+		});
+
+		this.completedArray = [];
+		this.pendingArray = [];
+		this.selectedUser = this.allTodoList.filter(res => res['userId'] == data.id);
+		this.selectedUser.forEach(res => {
+			(res['completed'] == true) ? this.completedArray.push(res) : this.pendingArray.push(res);
+		});
+		console.log('userDetails::::::43', this.selectedUser, this.completedArray, this.pendingArray);
+		this.graphJSON();
+	}
+
+	sortBy(array,type, property){
+		this.isDesc = !this.isDesc; //change the direction    
+		this.column = property;
+		this.type = type;
+		let direction = this.isDesc ? 1 : -1;
+
+		array.sort(function(a, b){
+			if(a[property] < b[property]){
+				return -1 * direction;
+			}
+			else if( a[property] > b[property]){
+				return 1 * direction;
+			}
+			else{
+				return 0;
+			}
+		});
+
+		if(type == 'completed'){
+			this.completedArray = [...array];
+		} else {
+			this.pendingArray = [...array];
+		}
+	};
+
+	graphJSON(){
+		this.selectUsergraphData = [];
+		const array = [{name:'Todo',data:[]}];
+		array[0].data.push( { y: this.completedArray.length, color: '#227306'});
+		array[0].data.push({ y: this.pendingArray.length, color: '#f4f70e'});
+		this.selectUsergraphData = [...array];
+	}
 }
